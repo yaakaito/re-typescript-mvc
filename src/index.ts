decf5ad65f760c152db649e903804e499f340332
KAZUMA Ukyo <yaakaito@gmail.com>
2013-04-26 17:55:49 +0900

    Add bootstrap

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,9 @@
 /// <reference path="./model/todo.ts" />
 /// <reference path="./controller/todo-controller.ts" />
 /// <reference path="./view/todo-view.ts" />
+
+$(() => {
+
+    new TodoApp.Layout();
+    new TodoApp.TodoController();
+});
